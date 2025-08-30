f01d26b4f0acd8828b0053d4207af86d2d39492b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-30 18:28:26 +0300

    Display the order lists image

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { offers } from "@/constants";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -10,8 +11,21 @@ export default function Index() {
         renderItem={({ item, index }) => {
           return (
             <View>
-              <Pressable className="bg-amber-600">
-                <Text>{item.title}</Text>
+              <Pressable
+                className="offer-card"
+                style={{ backgroundColor: item.color }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className={"size-full"}
+                        resizeMode={"contain"}
+                      />
+                    </View>
+                  </Fragment>
+                )}
               </Pressable>
             </View>
           );
