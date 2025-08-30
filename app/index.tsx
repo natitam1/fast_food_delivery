86eb450b498aa2f2434483bfac26360eee34337a
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-30 19:10:00 +0300

    Fix font path issues

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,22 @@
-import { offers } from "@/constants";
+import { images, offers } from "@/constants";
+import cn from "clsx";
 import { Fragment } from "react";
-import { FlatList, Image, Pressable, View } from "react-native";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 export default function Index() {
   return (
     <SafeAreaView>
       <FlatList
         data={offers}
         renderItem={({ item, index }) => {
+          const isEven: boolean = index % 2 === 0;
           return (
             <View>
               <Pressable
-                className="offer-card"
+                className={cn(
+                  "offer-card",
+                  isEven ? "flex-row-reverse" : "flex-row"
+                )}
                 style={{ backgroundColor: item.color }}
               >
                 {({ pressed }) => (
@@ -22,6 +26,17 @@ export default function Index() {
                         source={item.image}
                         className={"size-full"}
                         resizeMode={"contain"}
+                      />
+                    </View>
+                    <View className={"offer-card__info"}>
+                      <Text className="h1-bold text-white loading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        resizeMode="contain"
+                        tintColor="#ffffff"
                       />
                     </View>
                   </Fragment>
