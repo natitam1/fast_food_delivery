2ab87b288dc25e3888732cdfb47b80e0b635cd1b
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-30 18:14:23 +0300

    Add custom css codes

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,22 @@
-import { Text, View } from "react-native";
+import { offers } from "@/constants";
+import { FlatList, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white-100">
-      <Text className="text-5xl text-center text-blue-500 font-quicksand-bold">
-        Welcome to my React Native App!
-      </Text>
-    </View>
-    // <SafeAreaView>
-    //   <FlatList data={[]} renderItem={({ item }) => <div />} />
-    // </SafeAreaView>
+    <SafeAreaView>
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          return (
+            <View>
+              <Pressable className="bg-amber-600">
+                <Text>{item.title}</Text>
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
