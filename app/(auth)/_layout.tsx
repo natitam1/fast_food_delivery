aec1df56a893ede6bc46192e06820668cc3dcdc9
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 13:21:04 +0300

    Implement custom button and input in the sign in

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,5 @@
-import CustomButton from "@/components/CustomButton";
-import CustomInput from "@/components/CustomInput";
 import { images } from "@/constants";
+import { Slot } from "expo-router";
 import React from "react";
 import {
   Dimensions,
@@ -35,14 +34,7 @@ const _layout = () => {
             className="self-center size-48 absolute -bottom-16 z-10"
           />
         </View>
-        <CustomInput
-          placeholder="Enter your email"
-          value={""}
-          onChangeText={(text) => {}}
-          label="Email"
-          keyboardType="email-address"
-        />
-        <CustomButton />
+        <Slot />
       </ScrollView>
     </KeyboardAvoidingView>
   );
