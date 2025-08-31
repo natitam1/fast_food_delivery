c672e4889b6bb8bd7aedf2ff1c0f1513bb7cb977
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 19:42:15 +0300

    Fix font issues

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="h-screen w-screen items-center justify-center">
-      <Text className="text-5xl font-bold text-orange-600">
-        Edit app/index.tsx to edit this screen.
+      <Text className="text-5xl text-orange-600 font-quicksand-bold text-center">
+        Welcome to my React Native App.
       </Text>
     </View>
   );
