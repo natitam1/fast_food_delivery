55fca0135bd3cce2f297f1e2ff68f0c582851de0
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 20:17:00 +0300

    Edit the folder stracture

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const SingIn = () => {
+  return (
+    <View>
+      <Text>SingIn</Text>
+    </View>
+  );
+};
+
+export default SingIn;
