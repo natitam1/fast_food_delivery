3afc69a980c59f95c9d92729a0eb94fd682e328f
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 15:42:48 +0300

    Restart the fast_food_delivery app project

--- lib/appwrite.ts
+++
@@ -1,7 +0,0 @@
-export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  platform: ".com.nati.foodorder",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-  databaseId: "68b40acf003be881ffc9",
-  userCollectionId: "user",
-};
