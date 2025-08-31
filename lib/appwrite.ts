1360b82e7c199a8aa06358831446e883e97ae0ee
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 12:21:17 +0300

    Set up a collection using appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: ".com.nati.foodorder",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "68b40acf003be881ffc9",
+  userCollectionId: "user",
 };
