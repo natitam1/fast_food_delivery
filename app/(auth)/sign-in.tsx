2c4f44daf285e01adc906235972530d25b324bfc
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-31 13:31:55 +0300

    Finish the sign in UI

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,27 +1,58 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
-import { Link } from "expo-router";
-import React from "react";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    const { email, password } = form;
+
+    if (!email || !password)
+      return Alert.alert(
+        "Error",
+        "Please enter valid email address & password."
+      );
+
+    setIsSubmitting(true);
+
+    try {
+      // await signIn({ email, password });
+      Alert.alert("Success", "User signed in successfully.");
+
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+      // Sentry.captureEvent(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <View className="gap-10 bg-white rounded-lg p-5 mt-5">
       <CustomInput
         placeholder="Enter your email"
-        value={""}
-        onChangeText={(text) => {}}
+        value={form.email}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, email: text }));
+        }}
         label="Email"
         keyboardType="email-address"
       />
       <CustomInput
         placeholder="Enter your password"
-        value={""}
-        onChangeText={(text) => {}}
+        value={form.password}
+        onChangeText={(text) => {
+          setForm((prev) => ({ ...prev, password: text }));
+        }}
         label="Password"
         secureTextEntry={true}
       />
-      <CustomButton title="Sing In" />
+      <CustomButton title="Sing In" isLoading={isSubmitting} onPress={submit} />
       <View className="flex justify-center mt-5 flex-row gap-2">
         <Text className="base-regular text-gray-100">
           Don't have an account?
