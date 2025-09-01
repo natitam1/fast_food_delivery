1010f66e5b38a1a2751069996063c152d49cb461
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-01 14:58:03 +0300

    Create a tab bar at the bottom

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,19 @@
 import useAuthStore from "@/store/auth.store";
-import { Redirect, Slot } from "expo-router";
+import { Redirect, Slot, Tabs } from "expo-router";
 import React from "react";
 
 export default function TabLayout() {
   const { isAuthenticated } = useAuthStore();
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
-  return <Slot />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+        }}
+      />
+      <Slot />
+    </Tabs>
+  );
 }
