14a64b8bfe50369aa986177a902afb2c16f3c376
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-09-01 16:32:54 +0300

    Customize the tab bat icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,21 @@
+import { images } from "@/constants";
 import useAuthStore from "@/store/auth.store";
+import { TabBarIconProps } from "@/type";
 import { Redirect, Slot, Tabs } from "expo-router";
 import React from "react";
+import { Image, Text, View } from "react-native";
+
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+  <View className="tab-icon">
+    <Image
+      source={icon}
+      className="size-7"
+      resizeMode="contain"
+      tintColor={focused ? "#ef8c00" : "#5d5f6d"}
+    />
+    <Text>{title}</Text>
+  </View>
+);
 
 export default function TabLayout() {
   const { isAuthenticated } = useAuthStore();
@@ -11,6 +26,9 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.home} title="Home" />
+          ),
         }}
       />
       <Slot />
